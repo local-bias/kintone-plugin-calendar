0f353a870b079fdd99f4ee87b066d46c27cab5b0
Ribbit <echo07200503@gmail.com>
2023-11-13 09:15:15 +0900

    ✨ Add calendar plugin key and version to static.ts

--- src/common/static.ts
+++ src/common/static.ts
@@ -1,13 +1,16 @@
 import manifest from '../../plugin/manifest.json';
 
+export const PLUGIN_KEY = 'calendar';
 export const PLUGIN_NAME = manifest.name.ja;
+export const PLUGIN_VERSION = manifest.version;
 
-export const LOCAL_STORAGE_KEY = 'ribbit-kintone-plugin';
+export const LOCAL_STORAGE_PREFIX = 'ribbit-kintone-plugin-';
+export const LOCAL_STORAGE_KEY = `${LOCAL_STORAGE_PREFIX}${PLUGIN_KEY}`;
 
 export const URL_HOMEPAGE = manifest.homepage_url.ja;
-export const URL_TWITTER = 'https://twitter.com/LbRibbit';
-export const URL_GITHUB = 'https://github.com/Local-Bias';
+export const URL_INQUIRY = 'https://form.konomi.app';
 export const URL_PROMOTION = 'https://promotion.konomi.app/kintone-plugin';
 export const URL_BANNER = 'https://promotion.konomi.app/kintone-plugin/sidebar';
+export const URL_PLUGIN_LIST = 'https://ribbit.konomi.app/kintone-plugin';
 
 export const VIEW_ROOT_ID = 'ribbit-scheduler';
