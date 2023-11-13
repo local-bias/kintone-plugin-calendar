8cea9e9d0c34d39c3bb289c864d66d6af5060220
Ribbit <echo07200503@gmail.com>
2023-11-13 16:23:27 +0900

    🎨 Refactor sidebar component to use MUI Drawer

--- src/desktop/components/sidebar/index.tsx
+++ src/desktop/components/sidebar/index.tsx
@@ -1,73 +1,36 @@
-import styled from '@emotion/styled';
-import React, { FC, FCX, Suspense } from 'react';
-import { useRecoilCallback, useRecoilValue } from 'recoil';
-import { sidebarExpandedState } from '../../states/sidebar';
-import { Squash as Hamburger } from 'hamburger-react';
+import React, { FC, Suspense, useState } from 'react';
 import MenuIcon from '@mui/icons-material/Menu';
 
 import Categories from './categories';
-import { Fab, Tooltip } from '@mui/material';
+import { Drawer, Fab, Tooltip } from '@mui/material';
 
-const Component: FCX<{ expanded: boolean }> = ({ className, expanded }) => {
-  const onToggleHamburger = useRecoilCallback(
-    ({ set }) =>
-      () => {
-        set(sidebarExpandedState, (current) => !current);
-      },
-    []
-  );
+const Component: FC = () => {
+  const [open, setOpen] = useState(false);
+
+  const toggle = () => setOpen((prev) => !prev);
 
   return (
-    <div className={className}>
-      <div className='fixed right-8 bottom-4 z-10'>
+    <div>
+      <div className='fixed left-8 bottom-4 z-10'>
         <Tooltip title='サイドバーを表示'>
-          <Fab variant='circular' size='large' color='primary' onClick={onToggleHamburger}>
+          <Fab variant='circular' size='large' color='primary' onClick={toggle}>
             <MenuIcon />
           </Fab>
         </Tooltip>
       </div>
-      <div className='container'>
-        <Hamburger toggled={expanded} onToggle={onToggleHamburger} color='#718291' />
-        <div className='content'>
-          {expanded && (
-            <>
-              <Suspense fallback={null}>
-                <Categories />
-              </Suspense>
-            </>
-          )}
+      <Drawer anchor='left' open={open} onClose={toggle}>
+        <div className='p-8'>
+          <Suspense fallback={null}>
+            <Categories />
+          </Suspense>
         </div>
-      </div>
+      </Drawer>
     </div>
   );
 };
 
-const StyledComponent = styled(Component)`
-  transition: all 250ms ease;
-  width: ${({ expanded }) => (expanded ? 250 : 40)}px;
-
-  .container {
-    position: sticky;
-    top: 3rem;
-    height: calc(100vh - 3rem);
-  }
-
-  .content {
-    padding: 2rem 0.5rem;
-    color: #61717a;
-  }
-
-  img {
-    width: 100%;
-    height: auto;
-    opacity: 0.7;
-  }
-`;
-
 const Container: FC = () => {
-  const expanded = useRecoilValue(sidebarExpandedState);
-
-  return <StyledComponent expanded={expanded} />;
+  return <Component />;
 };
 
 export default Container;
--- src/desktop/states/sidebar.ts
+++ src/desktop/states/sidebar.ts
@@ -2,11 +2,6 @@ import { atom } from 'recoil';
 
 const PREFIX = 'sidebar';
 
-export const sidebarExpandedState = atom<boolean>({
-  key: `${PREFIX}sidebarExpandedState`,
-  default: false,
-});
-
 export const displayingCategoriesState = atom<string[] | null>({
   key: `${PREFIX}displayingCategoriesState`,
   default: null,
