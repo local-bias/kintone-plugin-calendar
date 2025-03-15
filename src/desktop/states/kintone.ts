df5b2fe1f2b9dcb2b70730a59d412324bc39ef8a
Ribbit <echo07200503@gmail.com>
2025-03-15 13:50:05 +0900

    ♻ recoil ➡ jotai

--- src/desktop/states/kintone.ts
+++ src/desktop/states/kintone.ts
@@ -1,69 +1,53 @@
 import { GUEST_SPACE_ID } from '@/lib/global';
 import { PREDEFINED_FIELDS } from '@/lib/kintone-rest-api';
 import { getSortedOptions } from '@/lib/utils';
+import { PluginCondition } from '@/schema/plugin-config';
 import { getAppId, getFormFields, kintoneAPI } from '@konomi-app/kintone-utilities';
-import { atom, selector } from 'recoil';
+import { atom } from 'jotai';
 
-const PREFIX = 'kintone';
+export const pluginConditionAtom = atom<PluginCondition | null>(null);
 
-export const pluginConditionState = atom<Plugin.Condition | null>({
-  key: `${PREFIX}pluginConditionState`,
-  default: null,
+export const loadingAtom = atom<boolean>(false);
+
+export const kintoneRecordsAtom = atom<kintoneAPI.RecordData[]>([]);
+
+export const appPropertiesAtom = atom<Promise<kintoneAPI.FieldProperties>>(async () => {
+  const { properties } = await getFormFields({
+    app: getAppId()!,
+    guestSpaceId: GUEST_SPACE_ID,
+    debug: process.env.NODE_ENV === 'development',
+  });
+
+  const filtered = Object.entries(properties).reduce<kintoneAPI.FieldProperties>(
+    (acc, [key, property]) => {
+      if (PREDEFINED_FIELDS.includes(property.type)) {
+        return acc;
+      }
+      return { ...acc, [key]: property };
+    },
+    {}
+  );
+  return filtered;
 });
 
-export const loadingState = atom<boolean>({
-  key: `${PREFIX}loadingState`,
-  default: false,
-});
+export const calendarEventCategoryAtom = atom<Promise<string[] | null>>(async (get) => {
+  const condition = get(pluginConditionAtom);
+  if (!condition?.calendarEvent.categoryField) {
+    return null;
+  }
 
-export const kintoneRecordsState = atom<kintoneAPI.RecordData[]>({
-  key: `${PREFIX}kintoneRecordsState`,
-  default: [],
-});
+  const properties = await get(appPropertiesAtom);
+  const categoryProperty: kintoneAPI.FieldProperty | undefined =
+    properties[condition.calendarEvent.categoryField];
 
-export const appPropertiesState = selector<kintoneAPI.FieldProperties>({
-  key: `${PREFIX}appPropertiesState`,
-  get: async () => {
-    const { properties } = await getFormFields({
-      app: getAppId()!,
-      guestSpaceId: GUEST_SPACE_ID,
-      debug: process.env.NODE_ENV === 'development',
-    });
+  if (
+    !categoryProperty ||
+    (categoryProperty.type !== 'CHECK_BOX' &&
+      categoryProperty.type !== 'RADIO_BUTTON' &&
+      categoryProperty.type !== 'DROP_DOWN')
+  ) {
+    return null;
+  }
 
-    const filtered = Object.entries(properties).reduce<kintoneAPI.FieldProperties>(
-      (acc, [key, property]) => {
-        if (PREDEFINED_FIELDS.includes(property.type)) {
-          return acc;
-        }
-        return { ...acc, [key]: property };
-      },
-      {}
-    );
-    return filtered;
-  },
-});
-
-export const calendarEventCategoryState = selector<string[] | null>({
-  key: `${PREFIX}calendarEventCategoryState`,
-  get: ({ get }) => {
-    const condition = get(pluginConditionState);
-    if (!condition?.calendarEvent.categoryField) {
-      return null;
-    }
-
-    const properties = get(appPropertiesState);
-    const categoryProperty: kintoneAPI.FieldProperty | undefined =
-      properties[condition.calendarEvent.categoryField];
-
-    if (
-      !categoryProperty ||
-      (categoryProperty.type !== 'CHECK_BOX' &&
-        categoryProperty.type !== 'RADIO_BUTTON' &&
-        categoryProperty.type !== 'DROP_DOWN')
-    ) {
-      return null;
-    }
-
-    return getSortedOptions(categoryProperty.options ?? {}).map((option) => option.label);
-  },
+  return getSortedOptions(categoryProperty.options ?? {}).map((option) => option.label);
 });
