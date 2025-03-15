ddb770df38b38ab91d477427b068ca650e912e0c
Ribbit <echo07200503@gmail.com>
2025-03-15 13:55:45 +0900

    ♻ recoil ➡ jotai

--- src/desktop/hooks/use-initialize.ts
+++ src/desktop/hooks/use-initialize.ts
@@ -1,59 +1,57 @@
-import { useEffect } from 'react';
-import { useRecoilCallback, useRecoilValue } from 'recoil';
-import { getCalendarEventFromKintoneRecord } from '../actions';
-import { calendarEventsState } from '../states/calendar';
-import { appPropertiesState, loadingState, pluginConditionState } from '../states/kintone';
-import { getAllRecordsWithId, getAppId, getQueryCondition } from '@konomi-app/kintone-utilities';
 import { GUEST_SPACE_ID } from '@/lib/global';
+import { PluginCondition } from '@/schema/plugin-config';
+import { getAllRecordsWithId, getAppId, getQueryCondition } from '@konomi-app/kintone-utilities';
+import { atom, useAtomValue, useSetAtom } from 'jotai';
+import { useEffect } from 'react';
+import { getCalendarEventFromKintoneRecord } from '../actions';
+import { calendarEventsAtom } from '../states/calendar';
+import { appPropertiesAtom, loadingAtom, pluginConditionAtom } from '../states/kintone';
+
+const handleRecordsInitializeAtom = atom(null, async (get, set, condition: PluginCondition) => {
+  try {
+    set(loadingAtom, true);
+    const app = getAppId()!;
+    const query = getQueryCondition() || '';
+    const fields = [
+      '$id',
+      condition.calendarEvent.inputTitleField,
+      condition.calendarEvent.startField,
+      condition.calendarEvent.allDayField,
+      condition.calendarEvent.noteField,
+      condition.calendarEvent.endField,
+    ].filter((field) => field);
+
+    if (condition.calendarEvent.categoryField) {
+      fields.push(condition.calendarEvent.categoryField);
+    }
+
+    const properties = await get(appPropertiesAtom);
+
+    const onStep: Parameters<typeof getAllRecordsWithId>[0]['onStep'] = ({ records }) => {
+      const calendarEvents = records.map((record) =>
+        getCalendarEventFromKintoneRecord({ condition, properties, record })
+      );
+      set(calendarEventsAtom, calendarEvents);
+    };
+
+    await getAllRecordsWithId({
+      app,
+      condition: query,
+      fields,
+      onStep,
+      guestSpaceId: GUEST_SPACE_ID,
+      debug: process.env.NODE_ENV === 'development',
+    });
+  } catch (error) {
+    console.error('レコードの取得に失敗しました', error);
+  } finally {
+    set(loadingAtom, false);
+  }
+});
 
 export const useInitialize = () => {
-  const condition = useRecoilValue(pluginConditionState);
-
-  const initialize = useRecoilCallback(
-    ({ set, snapshot }) =>
-      async (condition: Plugin.Condition) => {
-        try {
-          set(loadingState, true);
-          const app = getAppId()!;
-          const query = getQueryCondition() || '';
-          const fields = [
-            '$id',
-            condition.calendarEvent.titleField,
-            condition.calendarEvent.startField,
-            condition.calendarEvent.allDayField,
-            condition.calendarEvent.noteField,
-            condition.calendarEvent.endField,
-          ].filter((field) => field);
-
-          if (condition.calendarEvent.categoryField) {
-            fields.push(condition.calendarEvent.categoryField);
-          }
-
-          const properties = await snapshot.getPromise(appPropertiesState);
-
-          const onStep: Parameters<typeof getAllRecordsWithId>[0]['onStep'] = ({ records }) => {
-            const calendarEvents = records.map((record) =>
-              getCalendarEventFromKintoneRecord({ condition, properties, record })
-            );
-            set(calendarEventsState, calendarEvents);
-          };
-
-          await getAllRecordsWithId({
-            app,
-            condition: query,
-            fields,
-            onStep,
-            guestSpaceId: GUEST_SPACE_ID,
-            debug: process.env.NODE_ENV === 'development',
-          });
-        } catch (error) {
-          console.error('レコードの取得に失敗しました', error);
-        } finally {
-          set(loadingState, false);
-        }
-      },
-    []
-  );
+  const condition = useAtomValue(pluginConditionAtom);
+  const initialize = useSetAtom(handleRecordsInitializeAtom);
 
   useEffect(() => {
     if (condition) {
