976e66c666f518c313c6d230e09faae98561f9a0
Ribbit <echo07200503@gmail.com>
2025-03-16 13:13:37 +0900

    ♻ グローバル変数の定義を改善し、環境に応じたログ出力を追加

--- src/lib/global.ts
+++ src/lib/global.ts
@@ -3,5 +3,8 @@ import { detectGuestSpaceId } from '@konomi-app/kintone-utilities';
 export const PLUGIN_ID = kintone.$PLUGIN_ID;
 export const GUEST_SPACE_ID = detectGuestSpaceId() ?? undefined;
 
-process.env.NODE_ENV === 'development' &&
-  console.log('[plugin] Global variables have been redefined');
+export const ENV = (process?.env?.NODE_ENV ?? 'production') as 'production' | 'development';
+export const isProd = ENV === 'production';
+export const isDev = ENV === 'development';
+
+isDev && console.log('[plugin] Global variables have been redefined');
