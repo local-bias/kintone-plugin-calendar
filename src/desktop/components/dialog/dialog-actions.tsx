cab96ec603cbe256b39b3834c706e1f990dda608
Ribbit <echo07200503@gmail.com>
2025-03-16 12:42:20 +0900

    ♻ recoil ➡ jotai

--- src/desktop/components/dialog/dialog-actions.tsx
+++ src/desktop/components/dialog/dialog-actions.tsx
@@ -1,68 +1,14 @@
-import React, { FC } from 'react';
-import { useRecoilCallback, useRecoilValue } from 'recoil';
-import { produce } from 'immer';
-import { Button, DialogActions } from '@mui/material';
-import LoadingButton from '@mui/lab/LoadingButton';
 import SaveIcon from '@mui/icons-material/Save';
-
-import { dialogPropsState, dialogShownState } from '../../states/dialog';
-import { appPropertiesState, loadingState, pluginConditionState } from '../../states/kintone';
-import { calendarEventsState } from '../../states/calendar';
-import { addNewRecord, reschedule } from '../../actions';
+import LoadingButton from '@mui/lab/LoadingButton';
+import { Button, DialogActions } from '@mui/material';
+import { useAtomValue, useSetAtom } from 'jotai';
+import { FC } from 'react';
+import { handleDialogSubmitAtom } from '../../states/dialog';
+import { loadingAtom } from '../../states/kintone';
 
 const Component: FC<{ onDialogClose: () => void }> = ({ onDialogClose }) => {
-  const loading = useRecoilValue(loadingState);
-
-  const onEnterButtonClick = useRecoilCallback(
-    ({ reset, set, snapshot }) =>
-      async () => {
-        set(loadingState, true);
-        try {
-          const currentProps = await snapshot.getPromise(dialogPropsState);
-          const condition = await snapshot.getPromise(pluginConditionState);
-          const properties = await snapshot.getPromise(appPropertiesState);
-
-          if (currentProps.new) {
-            const newEvent = await addNewRecord({
-              calendarEvent: currentProps.event,
-              condition: condition!,
-              properties,
-            });
-            set(dialogPropsState, (current) =>
-              produce(current, (draft) => {
-                draft.event = newEvent;
-              })
-            );
-            set(calendarEventsState, (current) =>
-              produce(current, (draft) => {
-                const index = draft.findIndex((event) => event.id === currentProps.event.id);
-                draft[index] = newEvent;
-              })
-            );
-          } else {
-            await reschedule({
-              calendarEvent: currentProps.event,
-              condition: condition!,
-              properties,
-            });
-            set(calendarEventsState, (current) =>
-              produce(current, (draft) => {
-                const index = draft.findIndex((event) => event.id === currentProps.event.id);
-                draft[index] = currentProps.event;
-              })
-            );
-          }
-
-          reset(dialogShownState);
-          reset(dialogPropsState);
-        } catch (error) {
-          console.error(error);
-        } finally {
-          set(loadingState, false);
-        }
-      },
-    []
-  );
+  const loading = useAtomValue(loadingAtom);
+  const handleDialogSubmit = useSetAtom(handleDialogSubmitAtom);
 
   return (
     <DialogActions>
@@ -73,7 +19,7 @@ const Component: FC<{ onDialogClose: () => void }> = ({ onDialogClose }) => {
         loading={loading}
         loadingPosition='start'
         startIcon={<SaveIcon />}
-        onClick={onEnterButtonClick}
+        onClick={handleDialogSubmit}
         variant='contained'
       >
         決定
