f96e45f833b1639cf2ad25e1e52cd3942780cc98
Ribbit <echo07200503@gmail.com>
2025-03-16 12:42:56 +0900

    ♻ recoil ➡ jotai

--- src/desktop/components/dialog/inputs/start-end.tsx
+++ src/desktop/components/dialog/inputs/start-end.tsx
@@ -1,84 +1,90 @@
-import React, { FC, memo } from 'react';
-import { useRecoilCallback, useRecoilValue } from 'recoil';
-import { produce } from 'immer';
-import { dialogPropsState } from '../../../states/dialog';
-import { PluginCalendarEvent } from '../../../states/calendar';
-import { DateTimePicker } from '@/lib/components/datetime-picker';
-import { DateTime } from 'luxon';
 import { dateInputToDateTime, dateTimeToDateInput } from '@/desktop/actions';
-import { hasEndTimeState, hasStartTimeState } from '@/desktop/states/plugin';
+import { hasEndTimeAtom, hasStartTimeAtom } from '@/desktop/states/plugin';
 import { DatePicker } from '@/lib/components/date-picker';
+import { DateTimePicker } from '@/lib/components/datetime-picker';
+import { produce } from 'immer';
+import { atom, useAtomValue, useSetAtom } from 'jotai';
+import { DateTime } from 'luxon';
+import { FC } from 'react';
+import { dialogPropsAtom } from '../../../states/dialog';
 
-const Component: FC<{ start: PluginCalendarEvent['start']; end: PluginCalendarEvent['end'] }> =
-  memo((props) => {
-    const hasStartTime = useRecoilValue(hasStartTimeState);
-    const hasEndTime = useRecoilValue(hasEndTimeState);
+const handleStartChangeAtom = atom(null, async (get, set, date: DateTime | null) => {
+  set(dialogPropsAtom, (current) =>
+    produce(current, (draft) => {
+      if (date) {
+        draft.event.start = dateTimeToDateInput(date);
+      }
+    })
+  );
+});
 
-    const onStartChange = useRecoilCallback(
-      ({ set }) =>
-        (date: DateTime | null) => {
-          set(dialogPropsState, (current) =>
-            produce(current, (draft) => {
-              if (date) {
-                draft.event.start = dateTimeToDateInput(date);
-              }
-            })
-          );
-        },
-      []
-    );
+const handleEndChangeAtom = atom(null, async (get, set, date: DateTime | null) => {
+  set(dialogPropsAtom, (current) =>
+    produce(current, (draft) => {
+      if (date) {
+        draft.event.end = dateTimeToDateInput(date);
+      }
+    })
+  );
+});
 
-    const onEndChange = useRecoilCallback(
-      ({ set }) =>
-        (date: DateTime | null) => {
-          set(dialogPropsState, (current) =>
-            produce(current, (draft) => {
-              if (date) {
-                draft.event.end = dateTimeToDateInput(date);
-              }
-            })
-          );
-        },
-      []
-    );
+const DateEnd: FC = () => {
+  const props = useAtomValue(dialogPropsAtom);
+  const hasEndTime = useAtomValue(hasEndTimeAtom);
+  const onEndChange = useSetAtom(handleEndChangeAtom);
 
-    return (
-      <div>
-        {hasStartTime ? (
-          <DateTimePicker
-            ampm={false}
-            label='開始日時'
-            value={props.start ? dateInputToDateTime(props.start) : DateTime.local()}
-            onChange={onStartChange}
-          />
-        ) : (
-          <DatePicker
-            label='開始日'
-            value={props.start ? dateInputToDateTime(props.start) : DateTime.local()}
-            onChange={onStartChange}
-          />
-        )}
-        {hasEndTime ? (
-          <DateTimePicker
-            ampm={false}
-            label='終了日時'
-            value={props.end ? dateInputToDateTime(props.end) : DateTime.local()}
-            onChange={onEndChange}
-          />
-        ) : (
-          <DatePicker
-            label='終了日'
-            value={props.end ? dateInputToDateTime(props.end) : DateTime.local()}
-            onChange={onEndChange}
-          />
-        )}
-      </div>
-    );
-  });
+  return (
+    <>
+      {hasEndTime ? (
+        <DateTimePicker
+          ampm={false}
+          label='終了日時'
+          value={props.event.end ? dateInputToDateTime(props.event.end) : DateTime.local()}
+          onChange={onEndChange}
+        />
+      ) : (
+        <DatePicker
+          label='終了日'
+          value={props.event.end ? dateInputToDateTime(props.event.end) : DateTime.local()}
+          onChange={onEndChange}
+        />
+      )}
+    </>
+  );
+};
+
+const DateStart: FC = () => {
+  const { event } = useAtomValue(dialogPropsAtom);
+  const hasStartTime = useAtomValue(hasStartTimeAtom);
+  const onStartChange = useSetAtom(handleStartChangeAtom);
+
+  return (
+    <>
+      {hasStartTime ? (
+        <DateTimePicker
+          ampm={false}
+          label='開始日時'
+          value={event.start ? dateInputToDateTime(event.start) : DateTime.local()}
+          onChange={onStartChange}
+        />
+      ) : (
+        <DatePicker
+          label='開始日'
+          value={event.start ? dateInputToDateTime(event.start) : DateTime.local()}
+          onChange={onStartChange}
+        />
+      )}
+    </>
+  );
+};
 
 const Container: FC = () => {
-  const props = useRecoilValue(dialogPropsState);
-  return <Component start={props.event.start} end={props.event.end} />;
+  return (
+    <div>
+      <DateStart />
+      <DateEnd />
+    </div>
+  );
 };
 
 export default Container;
