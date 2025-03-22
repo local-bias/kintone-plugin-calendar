5bc9de77b91832b04ddc6457bbe403b5d2928023
Ribbit <echo07200503@gmail.com>
2025-03-23 02:32:15 +0900

    ♻ Jotaiを使用して配列操作フックを追加し、初期化処理をリファクタリング

--- src/config/hooks/use-initialize.ts
+++ src/config/hooks/use-initialize.ts
@@ -1,36 +1,40 @@
 import { GUEST_SPACE_ID } from '@/lib/global';
 import { getAppId, getViews } from '@konomi-app/kintone-utilities';
+import { atom, useSetAtom } from 'jotai';
+import { enqueueSnackbar } from 'notistack';
 import { useEffect } from 'react';
-import { useSetRecoilState } from 'recoil';
-import { allAppViewsState } from '../states/kintone';
-import { loadingCountState } from '../states/plugin';
+import { allAppViewsAtom } from '../states/kintone';
+import { loadingCountAtom } from '../states/plugin';
+
+export const initializeAppViewsAtom = atom(null, async (_, set) => {
+  try {
+    set(loadingCountAtom, (c) => c + 1);
+    const app = getAppId();
+    if (!app) {
+      throw new Error('アプリのフィールド情報が取得できませんでした');
+    }
+
+    const { views } = await getViews({
+      app,
+      preview: true,
+      guestSpaceId: GUEST_SPACE_ID,
+      debug: process.env.NODE_ENV === 'development',
+    });
+
+    set(allAppViewsAtom, views);
+  } catch (error) {
+    enqueueSnackbar('アプリのビュー情報を取得できませんでした', { variant: 'error' });
+  } finally {
+    set(loadingCountAtom, (c) => c - 1);
+  }
+});
 
 export const useInitialize = () => {
-  const setAllAppViews = useSetRecoilState(allAppViewsState);
-  const setLoading = useSetRecoilState(loadingCountState);
+  const initialize = useSetAtom(initializeAppViewsAtom);
 
   useEffect(() => {
-    (async () => {
-      try {
-        setLoading((c) => c + 1);
-        const app = getAppId();
-        if (!app) {
-          throw new Error('アプリのフィールド情報が取得できませんでした');
-        }
-
-        const { views } = await getViews({
-          app,
-          preview: true,
-          guestSpaceId: GUEST_SPACE_ID,
-          debug: process.env.NODE_ENV === 'development',
-        });
-
-        setAllAppViews(views);
-      } finally {
-        setLoading((c) => c - 1);
-      }
-    })();
-  });
+    initialize();
+  }, []);
 
   return {};
 };
