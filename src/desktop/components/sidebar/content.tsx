5443daa556c46aa8bf978851b5a04a14492e3023
Ribbit <echo07200503@gmail.com>
2025-03-22 15:19:51 +0900

    ✨ サイドバーコンポーネントに検索入力とカテゴリー表示を追加

---
+++ src/desktop/components/sidebar/content.tsx
@@ -0,0 +1,29 @@
+import { Suspense } from 'react';
+import Categories from './categories';
+import SidebarSearchInput from './search-input';
+
+export default function SidebarContent() {
+  // const toggle = useSetAtom(toggleSidebarExpandedAtom);
+  // const isMobile = useIsMobile();
+
+  return (
+    <div className='space-y-6'>
+      {/* <Button
+        variant='outline'
+        size='sm'
+        onClick={toggle}
+        className={cn('', {
+          'fixed z-10 top-4 left-4': isMobile,
+        })}
+      >
+        <PanelLeft className='w-5 h-5' />
+      </Button> */}
+      <SidebarSearchInput />
+      <div>
+        <Suspense fallback={null}>
+          <Categories />
+        </Suspense>
+      </div>
+    </div>
+  );
+}
