21063cabcc959ab14e491cbd0ecc854eace32b71
adachi <echo07200503@gmail.com>
2025-07-20 12:43:16 +0900

    📦 バージョンを0.18.0から0.18.1に更新

--- plugin.config.mjs
+++ plugin.config.mjs
@@ -23,7 +23,7 @@ export default /** @type { const } */ ({
   manifest: {
     base: {
       manifest_version: 1,
-      version: '0.18.0',
+      version: '0.18.1',
       type: 'APP',
       name: {
         en: 'calendar plugin',
