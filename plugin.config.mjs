6c66c594c3d7f209addb2a95193b23faae43ad75
adachi <echo07200503@gmail.com>
2025-12-13 15:39:20 +0900

    📦 remeda

--- plugin.config.mjs
+++ plugin.config.mjs
@@ -23,7 +23,7 @@ export default /** @type { const } */ ({
   manifest: {
     base: {
       manifest_version: 1,
-      version: '0.20.0',
+      version: '0.21.0',
       type: 'APP',
       name: {
         en: 'calendar plugin',
