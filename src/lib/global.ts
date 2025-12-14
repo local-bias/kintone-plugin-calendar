612778bcda159adf77c95f1b4bc5ae60098fb228
adachi <echo07200503@gmail.com>
2025-12-14 11:59:10 +0900

    🔧 他プラグインに合わせてグローバル変数を調整

--- src/lib/global.ts
+++ src/lib/global.ts
@@ -1,10 +1,17 @@
 import { detectGuestSpaceId } from '@konomi-app/kintone-utilities';
 
-export const PLUGIN_ID = kintone.$PLUGIN_ID;
-export const GUEST_SPACE_ID = detectGuestSpaceId() ?? undefined;
-
 export const ENV = (process?.env?.NODE_ENV ?? 'production') as 'production' | 'development';
 export const isProd = ENV === 'production';
 export const isDev = ENV === 'development';
 
-isDev && console.log('[plugin] Global variables have been redefined');
+export const PLUGIN_ID = kintone.$PLUGIN_ID;
+export const GUEST_SPACE_ID = detectGuestSpaceId() ?? undefined;
+export const LANGUAGE = kintone.getLoginUser()?.language;
+
+if (!isProd) {
+  console.log('[plugin] Global variables have been redefined', {
+    PLUGIN_ID,
+    GUEST_SPACE_ID,
+    LANGUAGE,
+  });
+}
